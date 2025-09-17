2967350c38d75feb6b3f86a32a4c02a69fcbce09
Doudou Fall <42393186+fdoudou@users.noreply.github.com>
2025-09-17 23:34:09 +0000

    first commit

---
+++ models/freelances.js
@@ -0,0 +1,164 @@
+const freelances = [
+	{
+		id: '13',
+		name: 'Samuel Ogbobo',
+		job: 'Intégrateur Web',
+		picture: 'http://localhost:8000/images/13.png',
+		skills: ['SEO','HTML','CSS','JS','Boostrap'],
+		location: 'Lagos',
+		available: true,
+		tjm: 500
+	},
+	{
+		id: '14',
+		name: 'Layla Ndiaye',
+		job: 'Designer',
+		picture: 'http://localhost:8000/images/14.png',
+		skills: ['Figma','Canva'],
+		location: 'Dakar',
+		available: true,
+		tjm: 400
+	},
+	{
+		id: '15',
+		name: 'Coumba Faye',
+		job: 'Développeuse frontend',
+		picture: 'http://localhost:8000/images/15.png',
+		skills: ['HTMX','React','Tailwind','WordPress'],
+		location: 'Dakar',
+		available: false,
+		tjm: 600
+	},
+	{
+		id: '16',
+		name: 'Doudou Fall',
+		job: 'Développeur fullstack',
+		picture: 'http://localhost:8000/images/16.png',
+		skills: ['Node JS', 'Express', 'React', 'PHP', 'SQL','HTML/CSS'],
+		location: 'Dakar',
+		available: false,
+		tjm: 600
+	},
+	{
+		id: '1',
+		name: 'Julien Brun',
+		job: 'Développeur mobile',
+		picture: 'http://localhost:8000/images/4.jpeg',
+		skills: ['React Native'],
+		location: 'Lyon',
+		available: true,
+		tjm: 500
+	},
+	{
+		id: '2',
+		name: 'Arielle Gautier',
+		job: 'Développeuse fullstack',
+		picture: 'http://localhost:8000/images/1.jpeg',
+		skills: ['Node JS', 'Vue.js', 'React', 'AWS'],
+		location: 'Paris',
+		available: false,
+		tjm: 620
+	},
+	{
+		id: '3',
+		name: 'Marine Carpentier',
+		job: 'Développeuse frontend',
+		picture: 'http://localhost:8000/images/2.jpeg',
+		skills: ['React', 'Gatsby', 'Next.js'],
+		location: 'Bordeaux',
+		available: true,
+		tjm: 520
+	},
+	{
+		id: '4',
+		name: 'Lucille Barre',
+		job: 'Product Designer',
+		picture: 'http://localhost:8000/images/3.jpeg',
+		skills: ['Figma', 'Webflow'],
+		location: 'Lille',
+		available: false,
+		tjm: 650
+	},
+	{
+		id: '5',
+		name: 'Clément Rolland',
+		job: 'Développeur mobile',
+		picture: 'http://localhost:8000/images/5.jpeg',
+		skills: ['IOS', 'Android'],
+		location: 'Lyon',
+		available: false,
+		tjm: 450
+	},
+	{
+		id: '6',
+		name: 'Grégoire Chevalier',
+		job: 'Développeur backend',
+		picture: 'http://localhost:8000/images/6.jpeg',
+		skills: ['Python', 'Django', 'Docker'],
+		location: 'Paris',
+		available: true,
+		tjm: 510
+	},
+	{
+		id: '7',
+		name: 'Raphaël Rodriguez',
+		job: 'Designer',
+		picture: 'http://localhost:8000/images/7.jpeg',
+		skills: ['Sketch', 'Illustrator'],
+		location: 'Paris',
+		available: true,
+		tjm: 480
+	},
+	{
+		id: '8',
+		name: 'Hugo Vysa',
+		job: 'Développeur frontend',
+		picture: 'http://localhost:8000/images/8.jpeg',
+		skills: ['SEO', 'Javascript Vanilla'],
+		location: 'Toulouse',
+		available: false,
+		tjm: 560
+	},
+	{
+		id: '9',
+		name: 'Mina Toman',
+		job: 'Développeuse Mobile',
+		picture: 'http://localhost:8000/images/11.jpeg',
+		skills: ['Android', 'React Native'],
+		location: 'Bayonne',
+		available: true,
+		tjm: 630
+	},
+	{
+		id: '10',
+		name: 'Amélie Leroy',
+		job: 'Développeuse backend',
+		picture: 'http://localhost:8000/images/12.jpeg',
+		skills: ['Node.js', 'Express', 'Docker'],
+		location: 'Paris',
+		available: false,
+		tjm: 400
+	},
+	{
+		id: '11',
+		name: 'Maxime Lebrun',
+		job: 'Intégrateur SEO',
+		picture: 'http://localhost:8000/images/9.jpeg',
+		skills: ['SEO'],
+		location: 'Rennes',
+		available: false,
+		tjm: 600
+	},
+	{
+		id: '12',
+		name: 'Raveesh Prashnav',
+		job: 'Développeur Mobile',
+		picture: 'http://localhost:8000/images/10.jpeg',
+		skills: ['Cordova','Ionic'],
+		location: 'Inde',
+		available: false,
+		tjm: 600
+	}
+]
+
+module.exports = freelances
